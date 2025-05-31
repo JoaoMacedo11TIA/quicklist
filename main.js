3b9c3db0d8590794da38ce94f558a8f8d66bb675
João Macedo <jpdm060509@gmail.com>
2025-05-31 10:54:33 -0300

    continuação da função e criar novos itens criando o objeto de cada item para ser adicioando ao array de items

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ const items = []
 function addItem() {
     alert("adicionar mais itens")
     const itemName = document.querySelector("#item").value
+
+    const item = {
+        name: itemName,
+        checked: false
+    }
+
+    items.push(item)
+
+    console.log(items)
+    
 }
